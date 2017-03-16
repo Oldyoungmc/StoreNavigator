0fd365f1ec359b48fb03155fb2a5aa95f3ab8ce2
Oldyoungmc <janriehm@dhcp13-40.bioinformatik.uni-saarland.de>
2017-03-16 17:32:19 +0100

    added helperalgos, minigraph, testlist

---
+++ StoreNavigator/src/HelperAlgorithms/HelperAlgorithms.java
@@ -0,0 +1,45 @@
+package HelperAlgorithms;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class HelperAlgorithms {
+
+	public HelperAlgorithms() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public LinkedList<String> getStringPermutations(String s){
+		LinkedList<String> list = new LinkedList<>();
+		if (s.length() > 0 && !s.isEmpty() && checkForSpecialChars(s)){
+			//TODO
+		}
+		
+		return list;
+	}
+	
+	public LinkedList<String> getMissingLetterWords(String s){
+		LinkedList<String> list = new LinkedList<>();
+		list.add(s.substring(1));
+		for (int i = 1; i < s.length(); i++){
+			if (i == s.length()-1){
+				list.add(s.substring(0, i-1));
+			}
+			else{
+				list.add(s.substring(0, i).concat(s.substring(i+1)));
+			}
+		}
+		return list;
+	}
+
+
+	private boolean checkForSpecialChars(String s){
+		LinkedList<String> specialChars = new LinkedList<>(Arrays.asList("!","@","$","#","%","^","&","*","(","(","_","+","=","[","{","]","}"));
+		for (String cha : specialChars){
+			if (s.contains(cha)){
+				return false;
+			}
+		}
+		return true;
+	}
+}
